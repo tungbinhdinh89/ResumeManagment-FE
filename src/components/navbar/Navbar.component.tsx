2728bd20f007181649b8c7e31a7d3c0739ef7ce1
Stephen Nguyen <tungbinhdinh89@gmail.com>
2024-02-23 23:16:48 +0700

    navbar

--- src/components/navbar/Navbar.component.tsx
+++ src/components/navbar/Navbar.component.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+import { useContext, useState } from "react";
 import "./navbar.scss";
+import { ThemeContext } from "../../context/theme.context";
 import { Link } from "react-router-dom";
+import { Menu, LightMode, DarkMode } from "@mui/icons-material";
+import { ToggleButton } from "@mui/material";
 
 const links = [
   { href: "/", label: "Home" },
@@ -10,22 +13,44 @@ const links = [
 ];
 
 function Navbar() {
+  const [open, setOpen] = useState<boolean>(false);
+  const { darkMode, toggleDarkMode } = useContext(ThemeContext);
+
+  const ToggleOpenMenu = () => {
+    setOpen((preState) => !preState);
+  };
+
+  const menuStyles = open ? "menu open" : "menu";
+
   return (
     <div className="navbar">
       <div className="brand">
         <span>Resume Management</span>
       </div>
-      <div className="menu">
+      <div className={menuStyles}>
         <ul>
           {links.map((item) => (
-            <li>
-              <Link to={item.href}>{item.label}</Link>
+            <li key={item.href}>
+              <Link to={item.href} onClick={ToggleOpenMenu}>
+                {item.label}
+              </Link>
             </li>
           ))}
         </ul>
       </div>
-      <div className="profile"></div>
-      <div className="toggle"></div>
+      <div className="hamburger">
+        <Menu onClick={ToggleOpenMenu} />
+      </div>
+
+      <div className="toggle">
+        <ToggleButton
+          value={"check"}
+          selected={darkMode}
+          onChange={toggleDarkMode}
+        >
+          {darkMode ? <LightMode /> : <DarkMode />}
+        </ToggleButton>
+      </div>
     </div>
   );
 }
