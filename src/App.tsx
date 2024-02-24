1dc57ed08840d3f1a30bbbb0cad970403300c7e2
Stephen Nguyen <tungbinhdinh89@gmail.com>
2024-02-24 22:13:32 +0700

    import lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
-import { useContext } from "react";
-import { ThemeContext } from "./context/theme.context";
+import { useContext, lazy, Suspense } from "react";
 import Navbar from "./components/navbar/Navbar.component";
+import { ThemeContext } from "./context/theme.context";
+import { Routes, Route } from "react-router-dom";
+import CustomeLinearProgress from "./components/custome-linear-progress/CustomeLinearProgress.component";
+import Companies from "./pages/home/companies/Companies.page";
+
+// Import with lazy loading
+const Home = lazy(() => import("./pages/home/Home.page"));
 
 const App = () => {
   const { darkMode } = useContext(ThemeContext);
@@ -10,7 +16,13 @@ const App = () => {
   return (
     <div className={appStyles}>
       <Navbar />
-      <div className="wrapper">Routes</div>
+      <div className="wrapper">
+        <Suspense fallback={<CustomeLinearProgress />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Suspense>
+      </div>
     </div>
   );
 };
