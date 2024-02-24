195891507466f686cf8f16617ac570cdfeb4d2f6
Stephen Nguyen <tungbinhdinh89@gmail.com>
2024-02-24 22:13:02 +0700

    add homepage

---
+++ src/components/custome-linear-progress/CustomeLinearProgress.component.tsx
@@ -0,0 +1,11 @@
+import { Box, LinearProgress } from "@mui/material";
+
+function CustomeLinearProgress() {
+  return (
+    <Box sx={{ width: "100%" }}>
+      <LinearProgress />
+    </Box>
+  );
+}
+
+export default CustomeLinearProgress;
---
+++ src/pages/home/Home.page.tsx
@@ -0,0 +1,27 @@
+import "./home.scss";
+
+function Home() {
+  return (
+    <div className="content home">
+      <h3>Welcome to website</h3>
+      <br />
+      <br />
+      <span>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero fugit
+        architecto voluptatum accusantium recusandae neque suscipit sed, vel
+        natus id maiores voluptatem quos? Neque quos pariatur consequuntur.
+        Officia nihil, itaque quia dolorem ea repellat necessitatibus esse
+        incidunt distinctio laboriosam quod cum voluptatem exercitationem
+        quibusdam maxime accusantium magnam amet architecto minus mollitia. Aut
+        non atque reprehenderit perferendis fuga optio repudiandae enim,
+        quibusdam ipsam! Soluta error sunt dolores inventore. Consequatur
+        doloremque error, animi voluptates omnis a qui delectus architecto
+        placeat? Possimus hic, laborum praesentium ducimus veniam dicta beatae
+        tempore repellat quisquam facilis autem ratione illo rem eius quis
+        mollitia assumenda! Deleniti, tempora?
+      </span>
+    </div>
+  );
+}
+
+export default Home;
